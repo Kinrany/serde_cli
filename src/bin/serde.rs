3eec802a18b6a4b8fbbfdf48b87a0236ae680727
Ruslan Fadeev <kinrany@yandex.com>
2022-01-05 20:46:01 +0300

    convert JSON into JSON

--- src/bin/serde.rs
+++ src/bin/serde.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use serde_transcode::transcode;
+use std::io::{stdin, stdout, Write};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut deserializer = serde_json::Deserializer::from_reader(stdin());
+    let mut serializer = serde_json::Serializer::pretty(stdout());
+
+    transcode(&mut deserializer, &mut serializer)?;
+    serializer.into_inner().flush()?;
+
+    Ok(())
+}
